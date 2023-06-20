7c1734af01d9447fc8788d244a917a94d1b4c263
tritri <sakaitri@gmail.com>
2023-06-20 20:56:30 +0900

    BluetoothのAdvertiseを取得できるよう実装（ただし特に使う予定なし）

--- AtomLiteBleDesktop/MainPage.xaml.cs
+++ AtomLiteBleDesktop/MainPage.xaml.cs
@@ -66,6 +66,7 @@ namespace AtomLiteBleDesktop
         public MainPage()
         {
             this.InitializeComponent();
+            advertist();
             DataContext = new
             {
                 textData = _textData,
@@ -83,7 +84,7 @@ namespace AtomLiteBleDesktop
             
 
             var bluetoothAccesser = (BluetoothAccesser)Application.Current.Resources["appBluetoothAccesserInstance"];
-
+            
             var result= await bluetoothAccesser.SearchDevices(servers,Dispatcher);
             if (result)
             {
@@ -96,6 +97,144 @@ namespace AtomLiteBleDesktop
                 await resourceInfos[0].StartSuspendAsync();
             }
 
+            // Attach a handler to process the received advertisement. 
+            // The watcher cannot be started without a Received handler attached
+            watcher.Received += OnAdvertisementReceived;
+            // Attach a handler to process watcher stopping due to various conditions,
+            // such as the Bluetooth radio turning off or the Stop method was called
+            watcher.Stopped += OnAdvertisementWatcherStopped;
+
+            watcher.Start();
+
+        }
+
+        /// <summary>
+        /// Invoked as an event handler when an advertisement is received.
+        /// </summary>
+        /// <param name="watcher">Instance of watcher that triggered the event.</param>
+        /// <param name="eventArgs">Event data containing information about the advertisement event.</param>
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {   
+            // We can obtain various information about the advertisement we just received by accessing 
+            // the properties of the EventArgs class
+
+            // The timestamp of the event
+            DateTimeOffset timestamp = eventArgs.Timestamp;
+
+            // The type of advertisement
+            BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
+
+            // The received signal strength indicator (RSSI)
+            Int16 rssi = eventArgs.RawSignalStrengthInDBm;
+
+            // The local name of the advertising device contained within the payload, if any
+            string localName = eventArgs.Advertisement.LocalName;
+
+            // Check if there are any manufacturer-specific sections.
+            // If there is, print the raw data of the first manufacturer section (if there are multiple).
+            string manufacturerDataString = "";
+            var manufacturerSections = eventArgs.Advertisement.ManufacturerData;
+            if (manufacturerSections.Count > 0)
+            {
+                // Only print the first one of the list
+                var manufacturerData = manufacturerSections[0];
+                var data = new byte[manufacturerData.Data.Length];
+                using (var reader = DataReader.FromBuffer(manufacturerData.Data))
+                {
+                    reader.ReadBytes(data);
+                }
+                // Print the company ID + the raw data in hex format
+                manufacturerDataString = string.Format("0x{0}: {1}",
+                    manufacturerData.CompanyId.ToString("X"),
+                    BitConverter.ToString(data));
+            }
+            Debug.WriteLine(string.Format("[{0}]: type={1}, rssi={2}, name={3}, manufacturerData=[{4}]",
+                    timestamp.ToString("hh\\:mm\\:ss\\.fff"),
+                    advertisementType.ToString(),
+                    rssi.ToString(),
+                    localName,
+                    manufacturerDataString));
+        }
+        
+        /// <summary>
+        /// Invoked as an event handler when the watcher is stopped or aborted.
+        /// </summary>
+        /// <param name="watcher">Instance of watcher that triggered the event.</param>
+        /// <param name="eventArgs">Event data containing information about why the watcher stopped or aborted.</param>
+        private async void OnAdvertisementWatcherStopped(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementWatcherStoppedEventArgs eventArgs)
+        {
+            // Notify the user that the watcher was stopped
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+            });
+        }
+
+        // The Bluetooth LE advertisement watcher class is used to control and customize Bluetooth LE scanning.
+        private BluetoothLEAdvertisementWatcher watcher;
+        /// <summary>
+        /// Advertisement受信処理初期化
+        /// </summary>
+        private void advertist()
+        {
+
+            // Create and initialize a new watcher instance.
+            watcher = new BluetoothLEAdvertisementWatcher();
+
+            // Begin of watcher configuration. Configure the advertisement filter to look for the data advertised by the publisher 
+            // in Scenario 2 or 4. You need to run Scenario 2 on another Windows platform within proximity of this one for Scenario 1 to 
+            // take effect. The APIs shown in this Scenario are designed to operate only if the App is in the foreground. For background
+            // watcher operation, please refer to Scenario 3.
+
+            // Please comment out this following section (watcher configuration) if you want to remove all filters. By not specifying
+            // any filters, all advertisements received will be notified to the App through the event handler. You should comment out the following
+            // section if you do not have another Windows platform to run Scenario 2 alongside Scenario 1 or if you want to scan for 
+            // all LE advertisements around you.
+
+            // For determining the filter restrictions programatically across APIs, use the following properties:
+            //      MinSamplingInterval, MaxSamplingInterval, MinOutOfRangeTimeout, MaxOutOfRangeTimeout
+
+            // Part 1A: Configuring the advertisement filter to watch for a particular advertisement payload
+
+            /*
+                // First, let create a manufacturer data section we wanted to match for. These are the same as the one 
+                // created in Scenario 2 and 4.
+                var manufacturerData = new BluetoothLEManufacturerData();
+
+                // Then, set the company ID for the manufacturer data. Here we picked an unused value: 0xFFFE
+                manufacturerData.CompanyId = 0xFFFE;
+
+                // Finally set the data payload within the manufacturer-specific section
+                // Here, use a 16-bit UUID: 0x1234 -> {0x34, 0x12} (little-endian)
+                var writer = new DataWriter();
+                writer.WriteUInt16(0x1234);
+
+                // Make sure that the buffer length can fit within an advertisement payload. Otherwise you will get an exception.
+                manufacturerData.Data = writer.DetachBuffer();
+
+                // Add the manufacturer data to the advertisement filter on the watcher:
+                watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
+
+            */
+            // Part 1B: Configuring the signal strength filter for proximity scenarios
+
+            // Configure the signal strength filter to only propagate events when in-range
+            // Please adjust these values if you cannot receive any advertisement 
+            // Set the in-range threshold to -70dBm. This means advertisements with RSSI >= -70dBm 
+            // will start to be considered "in-range".
+            watcher.SignalStrengthFilter.InRangeThresholdInDBm = -100;
+
+            // Set the out-of-range threshold to -75dBm (give some buffer). Used in conjunction with OutOfRangeTimeout
+            // to determine when an advertisement is no longer considered "in-range"
+            watcher.SignalStrengthFilter.OutOfRangeThresholdInDBm = -105;
+
+            // Set the out-of-range timeout to be 2 seconds. Used in conjunction with OutOfRangeThresholdInDBm
+            // to determine when an advertisement is no longer considered "in-range"
+            watcher.SignalStrengthFilter.OutOfRangeTimeout = TimeSpan.FromMilliseconds(2000);
+
+            // By default, the sampling interval is set to zero, which means there is no sampling and all
+            // the advertisement received is returned in the Received event
+
+            // End of watcher configuration. There is no need to comment out any code beyond this point.
 
         }
 
