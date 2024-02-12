52255b81b2a9e5ffccc48766f96c217ca6b454cb
tritri <sakaitri@gmail.com>
2024-02-12 20:10:36 +0900

    Serial通信用クラスを分離

---
+++ AtomLiteBleDesktop/SerialCommunication/SerialCommunicationTx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+namespace AtomLiteBleDesktop.SerialCommunication
+{
+    class SerialCommunicationTx
+    {
+        /*
+        private static SerialDevice device;
+
+        public static async void InitSerialPort()
+        {
+            string portName = "COM6";
+            string aqs = SerialDevice.GetDeviceSelector(portName);
+
+            var myDevices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(aqs, null);
+            if (myDevices.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                //TODO: 下記がnullになると落ちてしまうので対策が必要・・・
+                device = await SerialDevice.FromIdAsync(myDevices[0].Id);
+                if (device.IsRequestToSendEnabled)
+                {
+                    device.BaudRate = 115200;
+                    device.DataBits = 8;
+                    device.StopBits = SerialStopBitCount.One;
+                    device.Parity = SerialParity.None;
+                    device.Handshake = SerialHandshake.None;
+                    device.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                    device.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                }
+                device.ErrorReceived += SerialDeviceError;
+
+            }
+
+        }
+        */
+        public static async void DriveSerial()
+        {
+            string portName = "COM6";
+            string aqs = SerialDevice.GetDeviceSelector(portName);
+            SerialDevice serialDevice=null;
+            DataWriter dw=null;
+
+            try
+            {
+                var serialDeviceInfos = await DeviceInformation.FindAllAsync(aqs);
+                if (serialDeviceInfos != null)
+                {
+                    foreach (DeviceInformation serialDeviceInfo in serialDeviceInfos)
+                    {
+                        serialDevice = await SerialDevice.FromIdAsync(serialDeviceInfo.Id);
+                        if (serialDevice.IsRequestToSendEnabled)
+                        {
+                            serialDevice.BaudRate = 115200;
+                            serialDevice.DataBits = 8;
+                            serialDevice.StopBits = SerialStopBitCount.One;
+                            serialDevice.Parity = SerialParity.None;
+                            serialDevice.Handshake = SerialHandshake.None;
+                            serialDevice.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                            serialDevice.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                            serialDevice.ErrorReceived += SerialDeviceError;
+                            break;
+
+                        }
+
+                    }
+                }
+                if (serialDeviceInfos != null)
+                {
+                    using (dw = new DataWriter(serialDevice.OutputStream))
+                    {
+                        // Found a valid serial device.
+
+                        // Reading a byte from the serial device.
+                        //DataReader dr = new DataReader(serialDevice.InputStream);
+                        //int readByte = dr.ReadByte();
+
+                        // Writing a byte to the serial device.
+                        dw = new DataWriter(serialDevice.OutputStream);
+                        //dw.WriteByte(0x0a);
+                        dw.WriteString("a");
+                        var ret = dw.StoreAsync();
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                serialDevice.OutputStream.Dispose();
+                // Couldn't instantiate the device
+            }
+
+        }
+
+
+        public static void SerialDeviceError(SerialDevice sender, ErrorReceivedEventArgs e)
+        {
+            ;
+        }
+    }
+}
